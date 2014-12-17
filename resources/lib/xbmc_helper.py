9c68f4790ad99b29c1ad94f7a87904f23260ddd5
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2014-12-17 19:53:34 +0100

    Rewrite xbmc_helper

--- resources/lib/xbmc_helper.py
+++ resources/lib/xbmc_helper.py
@@ -3,176 +3,141 @@ import xbmc
 import helper
 
 
-def get_active_players_from_xbmc():
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'Player.GetActivePlayers', 'id': 1})
+def execute_rpc(**kargs):
+    """
+    :rtype : dict
+    """
+    kargs['jsonrpc'] = '2.0'
+    rpc_cmd = json.dumps(kargs)
 
-    result = xbmc.executeJSONRPC(rpccmd)
+    result = xbmc.executeJSONRPC(rpc_cmd)
     result = json.loads(result)
 
     if 'error' in result:
-        helper.debug("get_active_players_from_xbmc: " + str(result['error']))
+        helper.debug('execute_rpc: ' + str(result['error']))
         return None
 
+    return result['result']
+
+
+def get_active_players_from_xbmc():
+    result = execute_rpc(method='Player.GetActivePlayers', id=1)
+
     try:
-        return result['result'][0]['playerid']
+        return result[0]['playerid']
     except KeyError:
-        helper.debug("get_active_players_from_xbmc: KeyError: result['result']['playerid']")
         return None
 
 
 def get_currently_playing_from_xbmc(playerid):
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'Player.GetItem', 'params': {'playerid': playerid, 'properties': ['title']}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_currently_playing_from_xbmc: " + str(result['error']))
-        return None
+    result = execute_rpc(method='Player.GetItem', params={'playerid': playerid, 'properties': ['title']}, id=1)
 
     try:
-        return result['result']['item']
+        return result['item']
     except KeyError:
-        helper.debug("get_currently_playing_from_xbmc: KeyError: result['result']['movies']")
         return None
 
 
 def get_movies_from_xbmc():
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetMovies', 'params': {'properties': ['title', 'year', 'originaltitle', 'imdbnumber', 'playcount', 'lastplayed']}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_movies_from_xbmc: " + str(result['error']))
-        return None
+    """
+    :rtype : dict
+    """
+    result = execute_rpc(
+        method='VideoLibrary.GetMovies',
+        params={
+            'properties': ['title', 'year', 'originaltitle', 'imdbnumber', 'playcount', 'lastplayed']
+        },
+        id=1
+    )
 
     try:
-        return result['result']['movies']
+        return result['movies']
     except KeyError:
-        helper.debug("get_movies_from_xbmc: KeyError: result['result']['movies']")
         return None
 
 
 def get_tv_shows_from_xbmc():
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetTVShows', 'params': {'properties': ['title', 'year', 'imdbnumber', 'playcount', 'season', 'watchedepisodes']}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_tv_shows_from_xbmc: " + str(result['error']))
-        return None
+    result = execute_rpc(
+        method='VideoLibrary.GetTVShows',
+        params={
+            'properties': ['title', 'year', 'imdbnumber', 'playcount', 'season', 'watchedepisodes']
+        },
+        id=1
+    )
 
     try:
-        return result['result']
+        return result['tvshows']
     except KeyError:
-        helper.debug("get_tv_shows_from_xbmc: KeyError: result['result']")
         return None
 
 
 def get_seasons_from_xbmc(tvshow):
-    if not 'tvshowid' in tvshow:
+    if 'tvshowid' not in tvshow:
         return None
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetSeasons', 'params': {'tvshowid': tvshow['tvshowid'], 'properties': ['watchedepisodes']}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_seasons_from_xbmc: " + str(result['error']))
-        return None
+    result = execute_rpc(method='VideoLibrary.GetSeasons', params={'tvshowid': tvshow['tvshowid'], 'properties': ['watchedepisodes', 'season']}, id=1)
 
     try:
-        return result['result']
+        return result['seasons']
     except KeyError:
-        helper.debug("get_seasons_from_xbmc: KeyError: result['result']")
         return None
 
 
 def get_episodes_from_xbmc(tvshow, season):
-    if not 'tvshowid' in tvshow:
+    if 'tvshowid' not in tvshow:
         return None
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetEpisodes', 'params': {'tvshowid': tvshow['tvshowid'], 'season': season, 'properties': ['playcount', 'episode']}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_episodes_from_xbmc: " + str(result['error']))
-        return None
+    result = execute_rpc(method='VideoLibrary.GetEpisodes', params={'tvshowid': tvshow['tvshowid'], 'season': season, 'properties': ['playcount', 'episode']}, id=1)
 
     try:
-        return result['result']
+        return result['episodes']
     except KeyError:
-        helper.debug("get_episodes_from_xbmc: KeyError: result['result']")
         return None
 
 
 def get_movie_details_from_xbmc_by_title(title, year, fields):
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetMovieDetails', 'params': {'title': title, 'year': year, 'properties': fields}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_movie_details_from_xbmc_by_title: " + str(result['error']))
-        return None
+    result = execute_rpc(method='VideoLibrary.GetMovieDetails', params={'title': title, 'year': year, 'properties': fields}, id=1)
 
     try:
-        return result['result']['moviedetails']
+        return result['moviedetails']
     except KeyError:
-        helper.debug("get_movie_details_from_xbmc_by_title: KeyError: result['result']['moviedetails']")
         return None
 
 
 def get_movie_details_from_xbmc(library_id, fields):
     """ Get a single movie from xbmc given the id """
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetMovieDetails', 'params': {'movieid': library_id, 'properties': fields}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_movie_details_from_xbmc: " + str(result['error']))
-        return None
+    result = execute_rpc(method='VideoLibrary.GetMovieDetails', params={'movieid': library_id, 'properties': fields}, id=1)
 
     try:
-        return result['result']['moviedetails']
+        return result['moviedetails']
     except KeyError:
-        helper.debug("get_movie_details_from_xbmc: KeyError: result['result']['moviedetails']")
         return None
 
 
 def get_episode_details_from_xbmc(library_id, fields):
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetEpisodeDetails', 'params': {'episodeid': library_id, 'properties': fields}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_episode_details_from_xbmc: " + str(result['error']))
-        return None
+    result = execute_rpc(method='VideoLibrary.GetEpisodeDetails', params={'episodeid': library_id, 'properties': fields}, id=1)
 
     try:
-        return result['result']['episodedetails']
+        return result['episodedetails']
     except KeyError:
-        helper.debug("get_episode_details_from_xbmc: KeyError: result['result']['episodedetails']")
         return None
 
 
 def get_show_details_from_xbmc(library_id, fields):
-    rpccmd = json.dumps({'jsonrpc': '2.0', 'method': 'VideoLibrary.GetTVShowDetails', 'params': {'tvshowid': library_id, 'properties': fields}, 'id': 1})
-
-    result = xbmc.executeJSONRPC(rpccmd)
-    result = json.loads(result)
-
-    if 'error' in result:
-        helper.debug("get_show_details_from_xbmc: " + str(result['error']))
-        return None
+    result = execute_rpc(method='VideoLibrary.GetTVShowDetails', params={'tvshowid': library_id, 'properties': fields}, id=1)
 
     try:
-        return result['result']['tvshowdetails']
+        return result['tvshowdetails']
     except KeyError:
-        helper.debug("get_show_details_from_xbmc: KeyError: result['result']['tvshowdetails']")
         return None
+
+
+def set_movie_as_watched(movie_id):
+    execute_rpc(method='VideoLibrary.SetMovieDetails', params={'movieid': movie_id, "playcount": 1}, id=1)
+
+
+def set_movies_as_watched(movies):
+    """
+    Set movies as watched
+    :param movies: list of Movies
+    """
+    for m in movies:
+        set_movie_as_watched(m.id)
