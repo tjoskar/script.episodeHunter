249d1e65038448bc884000c01fb783a95c438ccc
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2014-12-27 21:39:22 +0100

    plays should always be treated as an int

--- resources/model/movie_model.py
+++ resources/model/movie_model.py
@@ -18,7 +18,7 @@ def create_from_xbmc(xbmc_movie):
     model.imdb_id = get_key(xbmc_movie, 'imdbnumber')
     model.title = xbmc_movie['originaltitle'] if 'originaltitle' in xbmc_movie else xbmc_movie['title']
     model.year = get_key(xbmc_movie, 'year')
-    model.plays = get_key(xbmc_movie, 'playcount', -1)
+    model.plays = int(get_key(xbmc_movie, 'playcount', -1))
     model.last_played = int(time.mktime(time.strptime(xbmc_movie['lastplayed'], '%Y-%m-%d %H:%M:%S'))) if 'lastplayed' in xbmc_movie else int(time.time())
     return model
 
