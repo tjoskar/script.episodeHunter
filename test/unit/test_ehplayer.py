317609bee10b166fa3b165d33d54ad33e8424323
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2014-12-27 20:50:56 +0100

    Added test case for EHPlayer

---
+++ test/unit/test_ehplayer.py
@@ -0,0 +1,47 @@
+from mock import patch, Mock
+import unittest
+
+from test.xbmc_base_test_case import XbmcBaseTestCase
+from test.mocks import xbmc_mock
+
+
+class GivenEHPlayer(XbmcBaseTestCase, object):
+    """
+    Test class for connections
+    """
+
+    xbmc = None
+    xbmcgui = None
+    http_mock = None
+    http = Mock()
+    addon = None
+
+    def setUp(self):
+        super(GivenEHPlayer, self).setUp()
+        self.xbmc = xbmc_mock
+        import addon
+        self.addon = addon
+
+    def test_should_return_true_if_movie(self):
+        data = {'type': 'movie'}
+        result = self.addon.is_movie(data)
+        self.assertTrue(result)
+
+    def test_should_return_false_if_not_movie(self):
+        data = {'type': 'episode'}
+        result = self.addon.is_movie(data)
+        self.assertFalse(result)
+
+    def test_should_return_true_if_episode(self):
+        data = {'type': 'episode'}
+        result = self.addon.is_episode(data)
+        self.assertTrue(result)
+
+    def test_should_return_false_if_not_episode(self):
+        data = {'type': 'movie'}
+        result = self.addon.is_episode(data)
+        self.assertFalse(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
